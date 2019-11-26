b0f8e2a3a84242a2db883399c6a33e9fc10e225e
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2019-11-26 16:29:45 +0700

    change on port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const bodyParser = require("body-parser")
-const PORT = 5001
+const PORT = 3000
 
 console.log(PORT)
 
